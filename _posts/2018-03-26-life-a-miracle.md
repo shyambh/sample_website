9ca5bbc69f17a502e025b3d09af58c337a165118
Shyam Bhattarai <bhattaraishyam@gmail.com>
2018-04-08 15:25:20 +0545

    Blog Update : Add posts

---
+++ _posts/2018-03-26-life-a-miracle.md
@@ -0,0 +1,25 @@
+---
+title: "Life : A Miracle"
+categories:
+  - poetry
+---
+
+Life is a miracle if you want it to be  
+Or, an endless tunnel, with no light you can see  
+But the sun, the moon and the air is the same  
+you can live in a moment's blush, or you can die again  
+
+I wish I was a mystic, high up on a hill  
+live in a little hut, silent and still  
+But would I find peace within, I shall never know  
+For I can never tell, which way my heart will go  
+
+Carry my blessings, my love, and carry my sins  
+be the sunshine in my eyes, where it all begins  
+Fools and Kings have traversed this dusty road forlorn  
+pretending to know it all, but the child has never grown  
+
+It has been written in the stars and in every grain of stand  
+the will of the universe, only a few can understand  
+For life is a miracle, happening every day  
+So live it up while you can, you'll die anyway  
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,11 @@
-var store = []
+var store = [{
+        "title": "Life : A Miracle",
+        "excerpt":"Life is a miracle if you want it to beOr, an endless tunnel, with no light you can seeBut the sun, the moon and the air is the sameyou can live in a moment’s blush, or you can die again I wish I was a mystic, high up on a...","categories": ["poetry"],
+        "tags": [],
+        "url": "http://localhost:4000/poetry/life-a-miracle/",
+        "teaser":"http://localhost:4000/assets/images/sunset.jpg"},{
+        "title": "अकिञ्चित, अर्धमुदित ति नयन",
+        "excerpt":"अकिञ्चित, अर्धमुदित ति नयनबिहानीको किरण जस्तैहृदयभित्र ब्यूँतिएका हजार सपनाहरु जस्तैआकाशका ति उज्याला हीराका टुक्राहरुकपालमा सिउरिएर उडिरहेकी तिमीविपनाभन्दा पनि सपनाझैँ बढी लाग्ने तिमीनाम पनि त भनिनौ बोलाउनलाईकारण पनि त छाडिनौ सम्झिरहनलाई अकिञ्चित, अर्धमुदित ति नयनरातको कोमल सुवास जस्तैतिमी कुनै नलेखिएको कविताको हरफजस्तैकुनै नागाईएको गज़लको बान्की जस्तैगाजल पनि त लगाईनौ रात भन्नलाईलाली...","categories": ["poetry"],
+        "tags": [],
+        "url": "http://localhost:4000/poetry/%E0%A4%85%E0%A4%95%E0%A4%BF%E0%A4%9E-%E0%A4%9A%E0%A4%BF%E0%A4%A4-%E0%A4%85%E0%A4%B0-%E0%A4%A7%E0%A4%AE%E0%A5%81%E0%A4%A6%E0%A4%BF%E0%A4%A4-%E0%A4%A4%E0%A4%BF-%E0%A4%A8%E0%A4%AF%E0%A4%A8/",
+        "teaser":"http://localhost:4000/assets/images/sunset.jpg"}]
